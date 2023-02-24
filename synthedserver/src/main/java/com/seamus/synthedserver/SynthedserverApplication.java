0b680a6d1610ef3222d6efd9629eb559ce6cf114
Séamus <ryans40@tcd.ie>
2023-02-24 11:40:16 +0000

    initial commit with boilerplate spring and react added

---
+++ synthedserver/src/main/java/com/seamus/synthedserver/SynthedserverApplication.java
@@ -0,0 +1,13 @@
+package com.seamus.synthedserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SynthedserverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SynthedserverApplication.class, args);
+	}
+
+}
---
+++ synthedserver/src/test/java/com/seamus/synthedserver/SynthedserverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.seamus.synthedserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SynthedserverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
