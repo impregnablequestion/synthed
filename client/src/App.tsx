c8a52e840e55f3a875f944250137427a0a21da92
Séamus Ryan <ryans40@tcd.ie>
2023-02-27 11:36:39 +0000

    Added a helloworld signal in App.ts

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,9 +3,24 @@ import { Counter } from './features/counter/Counter';
 import './App.css';
 
 function App() {
+
+  const context = new AudioContext();
+
+  const oscillator = context.createOscillator();
+  oscillator.type = "sine";
+  oscillator.frequency.value = 440;
+  const gain = context.createGain();
+
+  oscillator.connect(gain).connect(context.destination);
+
+  const handleClick = () => {
+    oscillator.start()
+  }
+
   return (
     <div className="App">
         <Counter />
+      <button onClick={handleClick}>play</button>
     </div>
   );
 }
