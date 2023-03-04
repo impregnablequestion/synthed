2f373e102a80d1791f24b802e6a95d34c5275e49
Séamus Ryan <ryans40@tcd.ie>
2023-03-04 12:11:58 +0000

    Making new generalised UI controls
    We have vertical and horizontal sliders, just need to make them work an a logarithmic scale rather than a linear scale

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,8 +2,10 @@ import './App.css';
 
 import MasterGainSlider from './components/display_components/MasterGainSlider';
 import WaveSelector from './components/display_components/WaveSelector';
-import Keyboard from './components/display_components/Keyboard';
+import Keyboard from './components/controls/Keyboard';
 import FilterFreqSlider from './components/display_components/FilterFreqSlider';
+import VertSlider from './components/controls/VerticalSlider';
+import HorizontalSlider from './components/controls/HorizontalSlider';
 
 function App() {
 
@@ -13,6 +15,8 @@ function App() {
       <WaveSelector />
       <MasterGainSlider />
       <FilterFreqSlider />
+      <VertSlider module='general' param="master_gain"  min={0} max={1} step={0.05}/>
+      <HorizontalSlider module='general' param="master_gain"  min={0} max={1} step={0.05}/>
     </div>
   );
 }
